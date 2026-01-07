5f3692cc03f409cc502c5745ed32f2e03e415fd4
om <omkumarsahu.26@gmail.com>
2026-01-08 00:47:49 +0530

    Leetcode problem 1 Two Sum

---
+++ leetcode-solutions/Two Sum/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        indices ={}
+        for i,num in enumerate(nums):
+            num2 = target - num
+            if num2 in indices:
+                return [indices[num2],i]
+            indices[num]=i
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.twoSum([2,7,11,15], 9))  # Output: [0, 1]
+    print(solution.twoSum([3,2,4], 6))      # Output: [1, 2]
+    print(solution.twoSum([3,3], 6))        # Output: [0, 1]
